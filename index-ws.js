81a944c614d6461caadff5539b1c384e53e1a02b
ernie <erniehightower@gmail.com>
2024-05-06 04:04:29 +0800

    web socket

--- index-ws.js
+++ index-ws.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = require('http').createServer();
 const app = express();
-const PORT = 3200;
+const PORT = 3000;
 
 app.get('/', function(req, res) {
   res.sendFile('index-ws.html', {root: __dirname});
@@ -10,4 +10,44 @@ app.get('/', function(req, res) {
 server.on('request', app);
 
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
+
+
+/** Websocket **/
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on('connection', function connection(ws) {
+  const numClients = wss.clients.size;
+
+  console.log('clients connected: ', numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send('welcome!');
+  }
+
+  ws.on('close', function close() {
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
+    console.log('A client has disconnected');
+  });
+
+  ws.on('error', function error() {
+    //
+  });
+});
+
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
+wss.broadcast = function broadcast(data) {
+  console.log('Broadcasting: ', data);
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+/** End Websocket **/
 
